c09de7892d398269ef5d448491de5ed7270054ea
arti-sukasem <51779010+arti-sukasem@users.noreply.github.com>
2022-03-29 21:21:20 +1300

    Update websites

---
+++ js/javaScript.js
@@ -0,0 +1,3 @@
+function popupWindow() {
+    window.open('https://mapcore-demo.org/current/scaffoldvuer/#/?url=https%3A%2F%2Fmapcore-bucket1.s3-us-west-2.amazonaws.com%2Fexamples%2Fbeating-heart%2Fbeating_heart_metadata.json&region=spiral%20colon', '_blank', 'top=100,left=200,height=400, width=600, toolbar=no, resizable=no')
+}
