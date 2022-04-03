84e2eb95008492021bbb60573035c8f7da3f11d9
arti-sukasem <51779010+arti-sukasem@users.noreply.github.com>
2022-04-03 19:39:40 +1200

    add google map api

--- js/javaScript.js
+++ js/javaScript.js
@@ -1,3 +1,19 @@
 function popupWindow() {
     window.open('https://mapcore-demo.org/current/scaffoldvuer/#/?url=https%3A%2F%2Fmapcore-bucket1.s3-us-west-2.amazonaws.com%2Fexamples%2Fbeating-heart%2Fbeating_heart_metadata.json&region=spiral%20colon', '_blank', 'top=100,left=200,height=400, width=600, toolbar=no, resizable=no')
 }
+
+// Initialize and add the map
+function initMap() {
+    // The location of Uluru -36.85783385252318, 174.79672305923748
+    const uluru = { lat:-36.85783385252318, lng: 174.79672305923748 };
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 6,
+      center: uluru,
+    });
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+      position: uluru,
+      map: map,
+    });
+  }
