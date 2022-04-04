c85c1ef55eec758f799603a38386fef7ec02a71c
arti-sukasem <51779010+arti-sukasem@users.noreply.github.com>
2022-04-04 20:02:22 +1200

    add Google API and gitignore

--- js/javaScript.js
+++ js/javaScript.js
@@ -1,3 +1,5 @@
+"use strict";
+
 function popupWindow() {
     window.open('https://mapcore-demo.org/current/scaffoldvuer/#/?url=https%3A%2F%2Fmapcore-bucket1.s3-us-west-2.amazonaws.com%2Fexamples%2Fbeating-heart%2Fbeating_heart_metadata.json&region=spiral%20colon', '_blank', 'top=100,left=200,height=400, width=600, toolbar=no, resizable=no')
 }
@@ -17,3 +19,4 @@ function initMap() {
       map: map,
     });
   }
+  
